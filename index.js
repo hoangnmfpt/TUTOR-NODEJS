bfda8f463cdb96130a278af55ad955eb06b3c090
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-20 22:42:07 +0700

    Lesson 3: CORS

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import router from "./routes/index.js";
 import mongoose from "mongoose";
+import cors from "cors";
 const PORT = 8000;
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/nodejs-xuong").then(() => {
--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    name: {
+    title: {
       type: String,
       required: true,
     },
@@ -17,9 +17,33 @@ const productSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    discountPercent: {
+    discountPercentage: {
       type: Number,
       default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    brand: {
+      type: String,
+      default: "No brand",
+    },
+    category: {
+      type: String,
+      default: "No category",
+    },
+    thumbnail: {
+      type: String,
+      default: "",
+    },
+    images: {
+      type: [String],
+      default: [],
     },
   },
   {
